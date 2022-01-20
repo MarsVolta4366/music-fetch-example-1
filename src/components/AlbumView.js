efe58d4b823f71a246cc9ba30b80fbb256f643df
unknown <derek.slauson5@gmail.com>
2022-01-19 20:14:55 -0600

    Did stuff

--- src/components/AlbumView.js
+++ src/components/AlbumView.js
@@ -1,16 +1,33 @@
-// These components will be making separate API calls from the app
-// component to serve specific data about a given album
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 const AlbumView = () => {
     const { id } = useParams()
     const [ albumData, setAlbumData ] = useState([])
 
+    useEffect(() => {
+        const fetchData = async () => {
+            const API_URL = `http://localhost:4000/song/${id}`
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setAlbumData(resData.results)
+        }
+        fetchData()
+    }, [id])
+
+    const justSongs = albumData.filter(entry => entry.kind === 'song')
+
+    const renderSongs = justSongs.map((song, i) => {
+        return (
+            <div key={i}>
+                <p>{song.trackName}</p>
+            </div>
+        )
+    })
+
     return (
         <div>
-            <h2>The id passed was: {id}</h2>
-            <p>Album Data Goes Here!</p>
+            {renderSongs}
         </div>
     )
 }
--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -1,13 +1,34 @@
 import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 
 function ArtistView() {
     const {id} = useParams()
     const [ artistData, setArtistData ] = useState([])
+
+    useEffect(() => {
+        const API_URL = `http://localhost:4000/album/${id}`
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setArtistData(resData.results)
+        }
+        fetchData()
+    }, [id])
+    console.log(artistData)
+    const justAlbums = artistData.filter(entry => entry.collectionType === "Album")
+    const renderAlbums = justAlbums.map((album, index) => {
+        return (
+            <div key={index}>
+                <Link to={`/album/${album.collectionId}`}>
+                    <p>{album.collectionName}</p>
+                </Link>
+            </div>
+        )
+    })
     return (
         <div>
-            <h2>The id passed was: {id} </h2>
-            <p>Artist Data Goes Here!</p>
+            <h2>List of Albums:</h2>
+            {renderAlbums}
         </div>
     )
 }
